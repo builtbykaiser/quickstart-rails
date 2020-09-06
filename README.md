81e3008af9f78f7d837dd3fca40828461be2e96b
Sergio Pantoja <2530982+sergiopantoja@users.noreply.github.com>
2020-09-06 16:07:08 -0500

    Add Font Awesome with sample icons

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 1. Click the "Use this template" button
 1. Clone your new repo into your local dev environment & `cd` into the new directory
 1. Change the [.ruby-version](https://github.com/builtbykaiser/quickstart-rails/blob/master/.ruby-version) if needed
+1. Ensure you have the npm configs set globally for Font Awesome Pro, or that you have a `.npmrc` file with your Font Awesome Pro auth token ([more info](https://fontawesome.com/how-to-use/on-the-web/setup/using-package-managers#installing-pro))
 1. Run `bundle install`
 1. Run `yarn install`
 1. Rename `.env.sample` to `.env` (`mv .env.sample .env`)
--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,6 +2,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 const chokidar = require('chokidar')
+const execa = require('execa')
 
 environment.config.devServer.before = (app, server) => {
   chokidar.watch([
@@ -10,6 +11,10 @@ environment.config.devServer.before = (app, server) => {
     'app/components/**/*.slim',
     'app/components/**/*.rb'
   ]).on('change', () => server.sockWrite(server.sockets, 'content-changed'))
+
+  chokidar.watch([
+    'config/font_awesome.json'
+  ]).on('change', () => execa('bin/generate-fa-icons'))
 }
 
 module.exports = environment.toWebpackConfig()
