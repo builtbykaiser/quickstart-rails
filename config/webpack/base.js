492ed533dcf614a8dd9d6aa1f1cc15f9082b20ad
Sergio Pantoja <2530982+sergiopantoja@users.noreply.github.com>
2021-06-08 13:50:28 -0500

    Upgrade gems and yarn dependencies

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -1,4 +1,5 @@
-const { webpackConfig, merge } = require('@rails/webpacker')
+const { webpackConfig } = require('@rails/webpacker')
+const { merge } = require('webpack-merge') // workaround for https://github.com/rails/webpacker/issues/2984
 
 const customConfig = {
   resolve: {
